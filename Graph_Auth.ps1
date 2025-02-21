34be18f653ad59d28fd0dec8003f16b3a7bfd3c6
Palmer2112 <60912173+Palmer2112@users.noreply.github.com>
2025-02-20 20:47:24 -0800

    Update Graph_Auth.ps1

--- Graph_Auth.ps1
+++ Graph_Auth.ps1
@@ -1,23 +1,21 @@
 $api = Read-Host "Enter Secret Key" -AsSecureString
 $token = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($api))
 $authparams = @{
-    ClientId    = ''
-    TenantId    = ''
+    ClientId = ''
+    TenantId = ''
     ClientSecret = $token
 }
 
 $auth = Get-MsalToken @authParams
 
 $graphGetParams = @{
-    Headers     = @{
-        'Authorization' = 'Bearer $($auth.AccessToken)'
-    'ConsistencyLevel' = 'eventual'
+    Headers= @{
+		Authorization = "Bearer $($auth.AccessToken)"
+		ConsistencyLevel = "eventual"
     }
-    Method      = "GET"
-  Uri = 'https://graph.microsoft.com/v1.0/me'
-  OutputType = 'PsObject'
+	Method = "GET"
+	Uri = 'https://graph.microsoft.com/v1.0/me'
+	OutputType = 'PsObject'
 }
 
-$Result = Invoke-RestMethod @graphGetParams
-
-$Result
+$Response = Invoke-RestMethod @graphGetParams
