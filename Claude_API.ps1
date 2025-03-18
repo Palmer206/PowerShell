6b6e750e3578c67469a911dfb79b187ce359fe74
Palmer206 <60912173+Palmer206@users.noreply.github.com>
2025-03-17 18:48:01 -0700

    Update Claude_API.ps1

--- Claude_API.ps1
+++ Claude_API.ps1
@@ -1,35 +1,35 @@
-<#
-----------| Set Variables |----------
-#>
-
 $model = "claude-3-7-sonnet-20250219"
 $anthropic_version = "2023-06-01"
 $max_tokens = 2048
 $key = Read-Host -AsSecureString | ConvertFrom-SecureString -AsPlainText
 
 $headers = @{
-	"x-api-key" = $key
+    "x-api-key" = $key
     "anthropic-version" = $anthropic_version
     "content-type" = "application/json"
 }
 
 function Post-ClaudeMessage {
+    param(
+        [string]$message
+    )
 
-	$messageContent = Read-Host "Enter Query"
-	
-	$body = @{
-		"model"  = $model
-		"max_tokens" = $max_tokens
-		"messages" = @(
-			@{
-				"role" = "user"
-				"content" = $messageContent
-			}	
-		)
-	} | ConvertTo-Json
-	
-	Invoke-RestMethod -Uri "https://api.anthropic.com/v1/messages" -Method Post -Headers $headers -Body $body
-	
+    if (!$message) {
+        $message = Read-Host "Enter Query"
+    }
+
+    $body = @{
+        "model"  = $model
+        "max_tokens" = $max_tokens
+        "messages" = @(
+            @{
+                "role" = "user"
+                "content" = $message
+            }   
+        )
+    } | ConvertTo-Json
+
+    Invoke-RestMethod -Uri "https://api.anthropic.com/v1/messages" -Method Post -Headers $headers -Body $body
 }
 
 $response = Post-ClaudeMessage
